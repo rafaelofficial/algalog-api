3108b8c70f34262a2cee49f75493e7a845d05537
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-10-21 20:29:23 -0300

    implementa método de consulta no banco de dados
    Corrigido também db.migration e adiciona versões V003 e V004 no arquivos .sql

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -4,27 +4,26 @@ import com.algaworks.algalog.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    /**
+     * PersistenceContext : A anotação PersistenceContext injeta um EntityManager na variável de instancia 'manager'
+     */
+    @PersistenceContext
+    private EntityManager manager;
+
+    /**
+     * @return : retorna uma consulta na tabela cliente no banco de dados
+     */
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-
-        var clienteUm = new Cliente();
-        clienteUm.setId(1L);
-        clienteUm.setNome("Zé da Silva");
-        clienteUm.setTelefone("11 98374-1111");
-        clienteUm.setEmail("zedasilva@algaworks.com");
-
-        var clienteDois = new Cliente();
-        clienteDois.setId(2L);
-        clienteDois.setNome("Maria da Silva");
-        clienteDois.setTelefone("11 98123-1111");
-        clienteDois.setEmail("mariadasilva@algaworks.com");
-
-        return Arrays.asList(clienteUm, clienteDois);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,14 +1,27 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
